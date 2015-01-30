3f66532b44f3e2135dfd79f1867256e1a359ce7f
Yves Hoppe <yves@compojoom.com>
2015-01-30 15:55:07 +0100

    Added coloriserString

--- source/libraries/compojoom/changelog/coloriser.php
+++ source/libraries/compojoom/changelog/coloriser.php
@@ -117,4 +117,95 @@ class CompojoomChangelogColoriser
 
 		return $html;
 	}
+
+	/**
+	 * Colorises a changelog text
+	 *
+	 * @param   string  $text      - path to the file
+	 * @param   bool    $onlyLast  - show only the info about the latest release
+	 *
+	 * @return string
+	 */
+	public static function coloriseString($text, $onlyLast = false)
+	{
+		$html = '';
+
+		$lines = explode("\n", $text);
+
+		array_shift($lines);
+
+		foreach ($lines as $line)
+		{
+			$line = trim($line);
+
+			if (empty($line))
+			{
+				continue;
+			}
+
+			$type = substr($line, 0, 1);
+
+			switch ($type)
+			{
+				case '=':
+					continue;
+					break;
+
+				case '*':
+					$html .= "\t" . '<li class="securityfixed"><span class="label label-critical">Fix</span>' . htmlentities(trim(substr($line, 2))) . "</li>\n";
+					break;
+
+				case '#':
+					$html .= "\t" . '<li class="fixed"><span class="label label-info">Fix</span>' . htmlentities(trim(substr($line, 2))) . "</li>\n";
+					break;
+
+				case '$':
+					$html .= "\t" . '<li class="language"><span class="label label-purple">Lang</span>' . htmlentities(trim(substr($line, 2))) . "</li>\n";
+					break;
+
+				case '+':
+					$html .= "\t" . '<li class="added"><span class="label label-success">New</span>' . htmlentities(trim(substr($line, 2))) . "</li>\n";
+					break;
+
+				case '^':
+					$html .= "\t" . '<li class="changed"><span class="label label-inverse">Diff</span>' . htmlentities(trim(substr($line, 2))) . "</li>\n";
+					break;
+
+				case '~':
+					$html .= "\t" . '<li class="changedmisc"><span class="label">Diff</span>' . htmlentities(trim(substr($line, 2))) . "</li>\n";
+					break;
+
+				case '-':
+					$html .= "\t" . '<li class="removed"><span class="label label-important">Del</span>' . htmlentities(trim(substr($line, 2))) . "</li>\n";
+					break;
+
+				case '!':
+					$html .= "\t" . '<li class="note"><span class="label label-warning">Note</span>' . htmlentities(trim(substr($line, 2))) . "</li>\n";
+					break;
+
+				default:
+					if (!empty($html))
+					{
+						$html .= "</ul>";
+
+						if ($onlyLast)
+						{
+							return $html;
+						}
+					}
+
+					if (!$onlyLast)
+					{
+						$html .= "<h4>" . preg_replace('#- Released.*#', '<span>$0</span>', $line) . "</h4>\n";
+					}
+
+					$html .= "<ul>\n";
+					break;
+			}
+		}
+
+		$html .= "</ul>";
+
+		return $html;
+	}
 }
