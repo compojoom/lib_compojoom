793d4d11a270d84ea69a73b39ce5e2b9f82f4bc3
Daniel Dimitrov <daniel@compojoom.com>
2015-04-13 17:26:18 +0200

    phpcs

--- source/libraries/compojoom/controller/jed.php
+++ source/libraries/compojoom/controller/jed.php
@@ -21,6 +21,8 @@ abstract class CompojoomControllerJed extends CompojoomController
 	 * Checks if the user has reviewed the component & if he hasn't, it asks him to review
 	 *
 	 * @throws Exception
+	 *
+	 * @return void
 	 */
 	public function reviewed()
 	{
@@ -28,20 +30,23 @@ abstract class CompojoomControllerJed extends CompojoomController
 		$jed = $config->get('jed', 0);
 		$result = '';
 
-		if($jed != 1)
+		if ($jed != 1)
 		{
 			$layout = new CompojoomLayoutFile('jed.jed');
+
 			if ($jed == 0)
 			{
-				$result =  $layout->render($this->data);
+				$result = $layout->render($this->data);
 			}
 			else
 			{
 				$now = JFactory::getDate();
 				$reminderSet = JFactory::getDate($jed);
 				$diff = $now->diff($reminderSet);
-				if($diff->days > 7) {
-					$result =  $layout->render($this->data);
+
+				if ($diff->days > 7)
+				{
+					$result = $layout->render($this->data);
 				}
 			}
 		}
@@ -58,14 +63,16 @@ abstract class CompojoomControllerJed extends CompojoomController
 	 * 2 = remind me in a week
 	 *
 	 * @throws Exception
+	 *
+	 * @return void
 	 */
 	public function update()
 	{
 		$config = JComponentHelper::getParams($this->component);
 		$jed = JFactory::getApplication()->input->getInt('jed', 1);
 
-		// the user wants a reminder
-		if($jed === 2)
+		// The user wants a reminder
+		if ($jed === 2)
 		{
 			$jed = JFactory::getDate()->toSql();
 		}
@@ -75,6 +82,6 @@ abstract class CompojoomControllerJed extends CompojoomController
 		// Store the updated config
 		CompojoomComponentHelper::updateConfiguration($this->component, $config);
 
-		$this->setRedirect('index.php?option='.$this->component);
+		$this->setRedirect('index.php?option=' . $this->component);
 	}
 }
