12386bd796a349a62522eae5aa67c73f10dc3098
Yves Hoppe <yves@compojoom.com>
2016-02-02 20:53:27 +0100

    Updated library to newest jorobo version

--- .drone/build.sh
+++ .drone/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "Started building at $(date)"
+
 # Update composer
 composer self-update
 
 # Install dependencies
 composer install
+
+cp jorobo.dist.ini jorobo.ini
 
 vendor/bin/robo build
