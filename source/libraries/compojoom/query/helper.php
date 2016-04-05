80a90e138b1aafcdde2e44c3bfbc3d336a9b6319
Daniel Dimitrov <daniel@compojoom.com>
2016-04-05 17:57:10 +0200

    4.1.1 # modal fixes + dbQueryHelper has implode function and createProperArrayforUpdate query

--- source/libraries/compojoom/query/helper.php
+++ source/libraries/compojoom/query/helper.php
@@ -31,13 +31,47 @@ class CompojoomQueryHelper
 	{
 		$n = ($not) ? " NOT " : "";
 
-		return $db->qn($column) . $n . ' IN (' .
-					implode(',', array_map(
-							function($v) use ($db){
-								return $db->q($v);
-							}, $array
-						)
-					) .
-		')';
+		return $db->qn($column) . $n . ' IN (' . self::implode($array, $db) . ')';
+	}
+
+	/**
+	 * Implode with value escaping
+	 *
+	 * @param   array   $values  - array with values to implode
+	 * @param   object  $db      - the db object
+	 * @param   string  $type    - the type of escaping we should do quote or quoteName (q or qn)
+	 *
+	 * @return string
+	 */
+	public static function implode($values, $db, $type = 'q')
+	{
+		return implode(',', array_map(
+				function ($v) use ($db, $type)
+				{
+					return $db->$type($v);
+				}, $values
+			)
+		);
+	}
+
+	/**
+	 * Create proper array for Update query. The initial array has the form of array ('key' => 'value')
+	 * the new array has the form of array( 'key = value', ...) both key and value are properly escaped
+	 *
+	 * @param   array   $array  - the array to format
+	 * @param   object  $db     - the db object
+	 *
+	 * @return array
+	 */
+	public static function createProperArrayForUpdateQuery($array, $db)
+	{
+		$set = array();
+
+		foreach ($array as $key => $value)
+		{
+			$set[] = $db->qn($key) . '=' . $db->q($value);
+		}
+
+		return $set;
 	}
 }
