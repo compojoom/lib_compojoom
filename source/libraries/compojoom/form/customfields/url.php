32d0bef42c83044aca3c4faefbd610c9606b6f64
Daniel Dimitrov <daniel@compojoom.com>
2018-02-25 08:48:14 +0100

    Improve the URL handling a little
    now if we have mailto - we display just the part after mailto:
    if we have http, https etc we display just the hots
    
    And we also do a wild guess - if the url starts with a www. then we are dealing with relative url

--- source/libraries/compojoom/form/customfields/url.php
+++ source/libraries/compojoom/form/customfields/url.php
@@ -20,7 +20,7 @@ class CompojoomFormCustomfieldsUrl
 	/**
 	 * Generates a xml string out of the field data
 	 *
-	 * @param   object  $data  - the field row
+	 * @param   object $data - the field row
 	 *
 	 * @return string
 	 */
@@ -35,13 +35,36 @@ class CompojoomFormCustomfieldsUrl
 	/**
 	 * There is no need to translate the value for text fields
 	 *
-	 * @param   object  $data              - the object with the field value
-	 * @param   string  $valueToTranslate  - the value for the field
+	 * @param   object $data             - the object with the field value
+	 * @param   string $valueToTranslate - the value for the field
 	 *
 	 * @return string
 	 */
 	public function render($data, $valueToTranslate)
 	{
-		return '<a href="' . $valueToTranslate . '" target="_blank" ref="nofollow">' . $valueToTranslate . '</a>';
+		$uri = Juri::getInstance($valueToTranslate);
+
+		// most probably external link if it starts with www. Add a scheme in order to properly redirect
+		if (substr($uri->toString(), 0, 4) === 'www.')
+		{
+			$uri->setScheme('http');
+		}
+
+		// wild guess, but if the host has a .com in it, then it's not a relative url
+
+		$displayUri = $uri->toString();
+
+		// If we are dealing with mailto, then output just path
+		if ($uri->getScheme() === 'mailto')
+		{
+			$displayUri = $uri->toString(array('path'));
+		}
+		// if we have a host, then output just the host
+		else if ($uri->getHost())
+		{
+			$displayUri = $uri->toString(array('host'));
+		}
+
+		return '<a href="' . $uri->toString() . '" target="_blank" rel="nofollow">' . $displayUri . '</a>';
 	}
 }
